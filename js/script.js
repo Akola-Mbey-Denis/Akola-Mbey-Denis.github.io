90b59a3bba548b0c8a6544e63de246b0e943ad47
akola <akoladenis97@gmail.com>
2020-10-07 15:34:55 +0000

    Update #6

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ window.onload = function() {
     document.getElementById("email").innerHTML = '<a href="mailto:' + 'akoladenis97@gmail.com</a>';
 
     var contactform = document.getElementById('contact-form');
-    contactform.setAttribute('action', '//formspree.io/' + 'hongwang9851' +
+    contactform.setAttribute('action', '//formspree.io/' + 'akoladenis97' +
         '@' + 'gmail' + '.' + 'com');
 }
 
