09780eaf4f271e8b26ced2c9a8adc67a65552464
akola <akoladenis97@gmail.com>
2020-10-07 15:47:50 +0000

    Update #7

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    document.getElementById("email").innerHTML = '<a href="mailto:' + 'akoladenis97@gmail.com</a>';
+    //document.getElementById("email").innerHTML = '<a href="mailto:' + 'akoladenis97@gmail.com</a>';
 
     var contactform = document.getElementById('contact-form');
     contactform.setAttribute('action', '//formspree.io/' + 'akoladenis97' +
